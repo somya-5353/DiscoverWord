31e821a4e4d25e55eb1ecfadda0a1dcfe976afd8
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-24 23:45:16 +0530

    initializing CocoaPods for the project

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+target 'DiscoverWord' do
+  use_frameworks!
+
+pod 'SVProgressHUD'
+
+end
---
+++ Pods/Target Support Files/Pods-DiscoverWord/Pods-DiscoverWord-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiscoverWord : NSObject
+@end
+@implementation PodsDummy_Pods_DiscoverWord
+@end
