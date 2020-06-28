899d337efde91c6d8a7e0a15886f8adaf22bd648
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-28 19:27:50 +0530

    logic for onboarding screen flow

--- DiscoverWord/View Controllers/Root View Controller/ViewController.swift
+++ DiscoverWord/View Controllers/Root View Controller/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var start: UIButton!
+    
+    var currentPageIndex: Int = 0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           scrollView.delegate = self
+       }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpUI()
     }
-
+    
+    func setUpUI() {
+        
+        if self.currentPageIndex == 0 {
+            self.topImage.image = UIImage(named: "onboard1")
+            self.topLabel.text = "Find definitions for more than 1,50,000 words"
+            self.start.isHidden = true
+        } else if self.currentPageIndex == 1 {
+            self.topImage.image = UIImage(named: "onboard2")
+            self.topLabel.text = "Find out synonyms, antonyms and similar words"
+            self.start.isHidden = true
+        } else if self.currentPageIndex == 2 {
+            self.topImage.image = UIImage(named: "onboard3")
+            self.topLabel.text = "Find hierarchical information related to the word and much more"
+            self.start.isHidden = false
+        }
+        self.topLabel.adjustsFontSizeToFitWidth = true
+        self.topLabel.numberOfLines = 0
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+    }
+    
 
 }
 
+extension ViewController:UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let pageIndex = Int(scrollView.contentOffset.x/UIScreen.main.bounds.size.width)
+        self.currentPageIndex = pageIndex
+        pageControl.currentPage = pageIndex
+        self.setUpUI()
+    }
+    
+}
